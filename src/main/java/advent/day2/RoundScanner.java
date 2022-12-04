8a892237e9720f0157c9c83aad8d74b33947a843
Will Morgan <wmorganjr@gmail.com>
2022-12-03 23:13:35 -0500

    Add RoundTuple and make RoundScanner an iterator for it

--- src/main/java/advent/day2/RoundScanner.java
+++ src/main/java/advent/day2/RoundScanner.java
@@ -1,12 +1,13 @@
 package advent.day2;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 // TODO this class can't implement Iterator<Round> yet because there are two
 //  ways to parse a round: day 1 or day 2. The proper thing to do will be to
 //  introduce a tuple class for which this class can be a proper Iterator,
 //  but that tuple class does not exist yet.
-final class RoundScanner {
+final class RoundScanner implements Iterator<RoundTuple> {
     private final Scanner scanner;
     private String[] lastScannedWords;
 
@@ -46,5 +47,17 @@ final class RoundScanner {
 
     private void nextRound() {
         lastScannedWords = scanner.nextLine().split(" ");
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNextRound();
+    }
+
+    @Override
+    public RoundTuple next() {
+        Round part1Round = nextRoundPart1();
+        Round part2Round = currentRoundPart2();
+        return new RoundTuple(part1Round, part2Round);
     }
 }
---
+++ src/main/java/advent/day2/RoundTuple.java
@@ -0,0 +1,4 @@
+package advent.day2;
+
+record RoundTuple(Round part1Round, Round part2Round) {
+}
