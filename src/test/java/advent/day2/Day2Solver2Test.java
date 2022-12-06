7d7bb622d4ab4706879a5ae04f0c4114f6f58c53
Will Morgan <wmorganjr@gmail.com>
2022-12-05 21:06:27 -0500

    Add day 2 alternate implementation

---
+++ src/test/java/advent/day2/Day2Solver2Test.java
@@ -0,0 +1,17 @@
+package advent.day2;
+
+import advent.Solution;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day2Solver2Test {
+
+    @Test
+    void solve() {
+        Solution solution = new Day2Solver2().solve();
+
+        assertEquals(11386, solution.answer1());
+        assertEquals(13600, solution.answer2());
+    }
+}
